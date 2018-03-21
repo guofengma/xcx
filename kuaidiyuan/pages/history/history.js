cb0ec32f53de94537c4363b50980ab0d7781c694
yudwcom <yudwcom@163.com>
2018-03-21 16:06:02 +0800

    快递员：发送完成

---
+++ kuaidiyuan/pages/history/history.js
@@ -0,0 +1,13 @@
+const APP_ID = 'wx2c985a73e416f727';//输入小程序appid  
+const APP_SECRET = 'cad42e76b18a5854f201b7ebceb024f4';//输入小程序app_secret  
+var OPEN_ID = ''//储存获取到openid  
+var SESSION_KEY = ''//储存获取到session_key  
+
+const app = getApp()
+
+
+
+Page({
+
+  
+})
